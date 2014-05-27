524761c4d3882dcb010e04926e48520389a8d4a5
Wutthichai <limplemoness@gmail.com>
2014-05-28 02:01:09 +0700

    remove bottom div

--- app/views/travel/index.blade.php
+++ app/views/travel/index.blade.php
@@ -7,7 +7,7 @@
 <head>
 
 	<!-- Your Basic Site Informations -->
-	<title>OHPhuket - รวบรวมสุดยอดข้อมูลท่องเที่ยวจังหวัดภูเก็ตมากมายที่นี่ !! </title>
+	<title>OhPhuket - รวบรวมสุดยอดข้อมูลท่องเที่ยวจังหวัดภูเก็ตมากมายที่นี่ !! </title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <meta name="description" content="Enews is a news or magazine site template that built with very cool responsive template with clean design, fast load, seo friendly, beauty color and a slew of features." />
     <meta name="keywords" content="Site Template, News, Magazine, Portofolio, HTML, CSS, jQuery, Newsletter, PHP Contact, Subscription, Responsive, Marketing, Clean, SEO" />
@@ -753,100 +753,17 @@
   </div> <!-- End Row-Fluid -->
  </div> <!-- End Container -->
  
- <div id="bottom">
-  <div class="container">
-   <div class="row-fluid">
-   
-    <div class="span3 widget clearfix">
-     <div class="header">
-      <h4>About Us</h4>
-     </div>
-     
-     <div class="content">
-      <p>Donec dignissim leo sit amet nisl luctus vestibulum. Morbi tortor arcu, aliquet nec aliquet at, convallis ut eros. Sed non tellus mauris, vitae congue ante.</p>
-      <p>Nunc imperdiet malesuada pharetra. Duis sodales, arcu tempus rutrum consectetur, sem ante auctor eros, in vehicula magna mauris sit amet est. Mauris ac eros arcu.</p>
-     </div>
-    </div> <!-- End Widget -->
-    
-    <div class="span3 widget clearfix">
-     <div class="tags-cloud">
-      
-      <div class="header">
-       <h4>Tags Cloud</h4>
-      </div>
-      
-      <ul>
-       <li><a href="blog_posts.html">windows</a></li>
-       <li><a href="blog_posts.html">apple</a></li>
-       <li><a href="blog_posts.html">software</a></li>
-       <li><a href="blog_posts.html">hardware</a></li>
-       <li><a href="blog_posts.html">gadget</a></li>
-       <li><a href="blog_posts.html">smartphone</a></li>
-       <li><a href="blog_posts.html">laptop</a></li>
-       <li><a href="blog_posts.html">business</a></li>
-       <li><a href="blog_posts.html">learn</a></li>
-       <li><a href="blog_posts.html">study</a></li>
-       <li><a href="blog_posts.html">game</a></li>
-       <li><a href="blog_posts.html">movie</a></li>
-       <li><a href="blog_posts.html">event</a></li>
-      </ul>
-      
-     </div>
-    </div> <!-- End Widget -->
-    
-    <div class="span3 widget clearfix">
-     <div class="tweets">
-      
-      <div class="header">
-       <h4>Latest Tweets</h4>
-      </div>
-      
-      <ul>
-       <li>
-        <span class="picons"><!-- --></span>
-        <p><a href="#">@realt0rlife</a> That's a very good question :) You may want to contact the author. There's a form on the right when you click their name.</p>
-        <span class="date">- 10 hours ago</span>
-       </li>
-       <li>
-        <span class="picons"><!-- --></span>
-        <p><a href="#">@ghostpool</a> Have you opened a support ticket yet? Can you send methe ticket number? Thanks!</p>
-        <span class="date">- 2 days ago</span>
-       </li>
-      </ul>
-      
-     </div>
-    </div> <!-- End Widget -->
-    
-    <div class="span3 widget clearfix">
-     <div class="flickr-photos">
-      
-      <div class="header">
-       <h4>Flickr Photos</h4>
-      </div>
-      
-      <ul id="flickr-gallery"><!-- --></ul>
-      
-     </div>
-    </div> <!-- End Widget -->
-   
-   </div> <!-- End Row-Fluid -->
-  </div> <!-- End Container -->
- </div> <!-- End Bottom -->
+ <div class="margin-top40"></div> <!-- End Bottom -->
   
  <div id="footer">
   <div class="container">
    
-   <p class="pull-left">Copyright 2013 Dots Theme&nbsp;&nbsp;|&nbsp;&nbsp;All Rights Reserved&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Dots Theme</a></p>
+   <p class="pull-left">Copyright 2013 OhPhuket.com&nbsp;&nbsp;|&nbsp;&nbsp;All Rights Reserved&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">OhPhuket.com</a></p>
    <ul class="social pull-right">
-    <li><a href="#" title="Youtube"><img src="{{ asset('theme/frontend/one/images/social/f0101/youtube.png') }}" alt="Youtube" /></a></li>
-    <li><a href="#" title="LinkedIn"><img src="{{ asset('theme/frontend/one/images/social/f0101/linkedin.png') }}" alt="LinkedIn" /></a></li>
-    <li><a href="#" title="Vimeo"><img src="{{ asset('theme/frontend/one/images/social/f0101/vimeo.png') }}" alt="Vimeo" /></a></li>
-    <li><a href="#" title="Tumblr"><img src="{{ asset('theme/frontend/one/images/social/f0101/tumblr.png') }}" alt="Tumblr" /></a></li>
-    <li><a href="#" title="Flickr"><img src="{{ asset('theme/frontend/one/images/social/f0101/flickr.png') }}" alt="Flickr" /></a></li>
-    <li><a href="#" title="DeviantArt"><img src="{{ asset('theme/frontend/one/images/social/f0101/deviantart.png') }}" alt="DeviantArt" /></a></li>
-    <li><a href="#" title="Delicious"><img src="{{ asset('theme/frontend/one/images/social/f0101/delicious.png') }}" alt="Delicious" /></a></li>
+    
+    
     <li><a href="#" title="Facebook"><img src="{{ asset('theme/frontend/one/images/social/f0101/facebook.png') }}" alt="Facebook" /></a></li>
-    <li><a href="#" title="Twitter"><img src="{{ asset('theme/frontend/one/images/social/f0101/twitter.png') }}" alt="Twitter" /></a></li>
+    <li><a href="#" title="Delicious"><img src="{{ asset('theme/frontend/one/images/social/f0101/delicious.png') }}" alt="Delicious" /></a></li>
    </ul>
    
   </div> <!-- End Container -->
